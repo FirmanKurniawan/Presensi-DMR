5e3532134270c27669aa4b69625847e98126e34c
FirmanKurniawan <kurniawan.firman94@gmail.com>
2023-11-08 11:06:13 +0700

    First Commit

--- database/migrations/2023_11_08_032451_create_presensis_table.php
+++ database/migrations/2023_11_08_032451_create_presensis_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('type');
-            $table->string('synchronize');
+            $table->string('synchronize')->nullable();
             $table->timestamps();
         });
     }
