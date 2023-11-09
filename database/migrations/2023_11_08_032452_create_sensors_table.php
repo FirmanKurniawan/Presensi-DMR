8ef27ffa1f3cd53829ed6fc063abd62621634980
FirmanKurniawan <kurniawan.firman94@gmail.com>
2023-11-09 11:53:55 +0700

    Update Table Migration

--- database/migrations/2023_11_08_032452_create_sensors_table.php
+++ database/migrations/2023_11_08_032452_create_sensors_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->float('heat_index');
             $table->timestampTz('time');
             $table->string('synchronize')->nullable()->default(null);
-            $table->timestamps();
         });
     }
 
